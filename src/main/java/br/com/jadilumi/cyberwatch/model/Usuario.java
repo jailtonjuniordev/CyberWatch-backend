eea5f4d53eef74e46e1fd69550b6bb4bcbfb6db7
jailtonjuniordev <jailtonjr99@hotmail.com.br>
2024-05-01 14:32:15 -0300

    FEAT MOD: Alteração de campos do model de Usuario

--- src/main/java/br/com/jadilumi/cyberwatch/model/Usuario.java
+++ src/main/java/br/com/jadilumi/cyberwatch/model/Usuario.java
@@ -1,17 +1,14 @@
 package br.com.jadilumi.cyberwatch.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.NonNull;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table(name = "usuario")
 public class Usuario extends AbstractAuditingEntity {
 
@@ -21,28 +18,18 @@ public class Usuario extends AbstractAuditingEntity {
 
     @NonNull
     @Column(length = 50, unique = true, nullable = false)
-    private String login;
+    private String username;
 
     @NonNull
     @Column(name = "nome", length = 100, nullable = false)
     private String nome;
 
-    @Column(length = 100)
-    private String email;
-
     @NonNull
     @Column(length = 100)
     private String senha;
 
-    @Column(length = 11)
-    private String cpf;
-
     @Column(length = 1)
-    private String genero;
-
-    @NonNull
-    @Column(name = "aniversario", length = 10)
-    private String aniversario;
+    private Character genero;
 
     @Column(name = "telefone", length = 15)
     private String telefone;
---
+++ src/main/java/br/com/jadilumi/cyberwatch/service/UsuarioService.java
@@ -0,0 +1,9 @@
+package br.com.jadilumi.cyberwatch.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+}
