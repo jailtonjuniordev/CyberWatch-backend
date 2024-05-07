bd81ff7409e5e9ebd76b2e7cd963cf861157c890
Jailton Junior <79541508+jailtonjuniordev@users.noreply.github.com>
2024-05-06 21:23:26 -0300

    Update UsuarioController.java

--- src/main/java/br/com/jadilumi/cyberwatch/controller/UsuarioController.java
+++ src/main/java/br/com/jadilumi/cyberwatch/controller/UsuarioController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin(value = "*", maxAge = 3600)
 public class UsuarioController {
 
     @Autowired
