7f09b51fe6121888afea15c9565cc07124a0876d
Jailton Junior <79541508+jailtonjuniordev@users.noreply.github.com>
2024-05-06 21:23:14 -0300

    Update LoginController.java

--- src/main/java/br/com/jadilumi/cyberwatch/controller/LoginController.java
+++ src/main/java/br/com/jadilumi/cyberwatch/controller/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(value = "*", maxAge = 3600)
 public class LoginController {
 
     @Autowired
